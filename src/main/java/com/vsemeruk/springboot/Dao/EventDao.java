90bda3f1dec4c0c33639607567f249560df35b17
Vitaliy Sam <vitaliy.semeruk@beskydsoft.com>
2018-06-01 19:42:56 +0300

    Fix bug with ids

--- src/main/java/com/vsemeruk/springboot/Dao/EventDao.java
+++ src/main/java/com/vsemeruk/springboot/Dao/EventDao.java
@@ -18,10 +18,10 @@ public class EventDao {
 
             {
                 put(1, new Event(1, "Java MeetUp IF", "2/09/18", "18:00", "IF, Sakharova 23"));
-                put(2, new Event(1, "Italian Pizza CookBook MeetUp", "7/09/18", "18:00", "IF, Pryvokzalna 13"));
-                put(3, new Event(1, "HairCut in 2018", "18/09/18", "14:00", "IF, Parkova 3"));
-                put(4, new Event(1, "JEEConf 2018", "4/09/18", "17:00", "IF, Pchilky 37"));
-                put(5, new Event(1, "Android Toolset IF", "22/09/18", "17:30", "IF, Moroza 27"));
+                put(2, new Event(2, "Italian Pizza CookBook MeetUp", "7/09/18", "18:00", "IF, Pryvokzalna 13"));
+                put(3, new Event(3, "HairCut in 2018", "18/09/18", "14:00", "IF, Parkova 3"));
+                put(4, new Event(4, "JEEConf 2018", "4/09/18", "17:00", "IF, Pchilky 37"));
+                put(5, new Event(5, "Android Toolset IF", "22/09/18", "17:30", "IF, Moroza 27"));
             }
 
         };
